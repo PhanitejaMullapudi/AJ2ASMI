c7d98e4caa6bf013d5b958f1753c2954f354a12d
PhaniTeja Mullapudi <Phaniteja.M@pbsystems.biz>
2017-04-13 19:07:11 +0530

    gaurd added

---
+++ app/Shared/Header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'header',
+    templateUrl:'app/Shared/Header/header-component.html'
+})
+export class HeaderCompoent {
+    
+}
